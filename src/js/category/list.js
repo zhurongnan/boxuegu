5c032e795994de3617fc8160370ee95b640b778b
azhu <zrn3324617160@163.com>
2017-09-11 21:58:16 +0800

    学科列表更新

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,6 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+$.get("/v6/category", function(data) {
+    $('.table-bordered').append(template('category-list-tpl', data.result))
+})
