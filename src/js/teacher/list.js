cf6afda46dd38302ab89f8b946f70481897e1a2f
azhu <zrn3324617160@163.com>
2017-09-13 00:48:26 +0800

    修复讲师管理

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,2 +1,22 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+$.get("/v6/teacher", function(data) {
+    data.code == 200 && $('#teacher-list-table').append(template('teacher-list-tpl', data.result))
+})
+
+
+$(document).on('click', 'btn-teacher-status', function() {
+    var $this = $(this);
+    var data = {
+        tc_id: $(this).attr('data-id'),
+        tc_status: $(this).attr('data-status')
+    }
+    $.post('/v6/teacher/handle', data, function(data) {
+        var newStatus = data.result.tc_status;
+        $this.text(newStatus == 0 ? '注销' : '启动');
+        $this.attr('data-status', 'newStatus')
+
+
+    })
+})
