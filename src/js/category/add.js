95ed089a81e1df66e24c3c1043294f8b2799423b
azhu <zrn3324617160@163.com>
2017-09-12 10:46:00 +0800

    until

--- src/js/category/add.js
+++ src/js/category/add.js
@@ -1,2 +1,19 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+// 请求数据，渲染模板，插入指定位置的坑
+$.get("/v6/category/top", function(data) {
+    if (data.code == 200) {
+        $('#category-top-select').html(template('select_tpl', data.result));
+    }
+});
+
+// + * 表单提交：
+//  + * ajaxForm方法会判断你传入的数据类型，如果是对象认为是配置，函数认为是成功回
+
+$('#category-add-form').ajaxForm(function(data) {
+
+    if (data.code == 200) {
+        alert('恭喜你，创建了一个新学科');
+    }
+})
